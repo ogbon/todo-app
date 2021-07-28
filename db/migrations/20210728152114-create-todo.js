de769ed5af7209c2bed44538284046b28b4bd099
ogbon <onethinneedful@gmail.com>
2021-07-28 16:42:33 +0100

    add models and migration files

---
+++ db/migrations/20210728152114-create-todo.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      done: {
+        type: Sequelize.BOOLEAN
+      },
+      detail: {
+        type: Sequelize.STRING
+      },
+      dueDate: {
+        type: Sequelize.DATE
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'user_id'
+       }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Todos');
+  }
+};
